ff10b6af54372f72641fbbfbd85b65afccc1dd96
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-10-29 17:23:49 -0400

    fixed unfollow and follow button

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,5 @@
 class DashboardsController < ApplicationController
 	def show
-		@text_spot = TextSpot.new
-		@photo_spot = PhotoSpot.new
-		@spots = current_user.spots
+		
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,28 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :spots
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+
+  has_many :followed_user_relationships,
+   foreign_key: :follower_id, 
+   class_name: 'FollowingRelationship'
+
+  has_many :followed_users, through: :followed_user_relationships
+
+  has_many :follower_relationships,
+   foreign_key: :follower_user_id,
+   class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
+
+  def following? user
+  	followed_user_ids.include? user.id
+  end
+
+  def follow user
+  	followed_users << user
+  end
+
+  def unfollow user
+  	followed_users.delete(user)
+  end
+
 end
