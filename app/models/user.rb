fab3d971449d2782a308930ccb9c186f0f3f36a9
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-10-29 15:44:52 -0400

    added followed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :spots
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'homes#show'
   resource :dashboard, only:[:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+  	post 'follow' => 'following_relationships#create'
+  end
   resources :spots, only: [:show]
   resources :text_spots, only: [:create]
   resources :photo_spots, only: [:create]
